fde43b81e01b874ec8719ba80a978b5eb480145b
adn87 <aaghadieh@gmail.com>
2023-11-07 09:22:03 -0500

    Password_Manager_003
    - when the use inputs website, email, and password. The data that gets entered is saved in a .txt file

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from tkinter import *
+import pandas
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {username} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -17,8 +30,10 @@ canvas.grid(row=0, column=1)
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 username_entry = Entry(width=35)
 username_entry.grid(row=2, column=1, columnspan=2)
+username_entry.insert(0, "dummy@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
@@ -33,7 +48,7 @@ password_label.grid(row=3, column=0)
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
