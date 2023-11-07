54af71771552d2ded0467a58632d73bc12d06571
adn87 <aaghadieh@gmail.com>
2023-11-07 09:34:30 -0500

    Password_Manager_004
    - generate Dialog Boxes and Pop-ups using messagebox class from Tkinter.
    - checked whether the user left any box empty or not.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import pandas
+from tkinter import messagebox
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -11,10 +11,16 @@ def save():
     username = username_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {username} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="HOLD ON", message="Make sure the fields are empty ")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {username} "
+                                                              f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {username} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
