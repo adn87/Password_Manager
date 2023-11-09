87a071c477465fb6fd63694b7f7abc24553ed75d
adn87 <aaghadieh@gmail.com>
2023-11-09 03:49:18 -0500

    Password_Manager_008
    - created a Search button so the user can click it to get the password for the website they are trying to log into in the JSON data file .
    - Added exception handler, if the user tries to search for a website, and there's no JSON file the program won't crash it will instead pop a message saying there's no data on file. Also, if the user tries to search for a website not in the database the program will pop a message saying that website doesn't exist in the database.

--- main.py
+++ main.py
@@ -28,6 +28,22 @@ def generate_password():
     pyperclip.copy(password)
 
 
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="ERROR", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="ERROR", message=f"no details for {website} exists")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
@@ -36,8 +52,8 @@ def save():
     password = password_entry.get()
     new_data = {
         website: {
-            "email:": username,
-            "password:": password
+            "email": username,
+            "password": password
         }
     }
 
@@ -65,16 +81,16 @@ def save():
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 username_entry = Entry(width=35)
 username_entry.grid(row=2, column=1, columnspan=2)
@@ -95,5 +111,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
